1594f6739dc421ba08145ffd3adbace266e8baed
arsyadshidiq <51278206+arsyadshidiq@users.noreply.github.com>
2019-07-09 13:19:23 +0700

    Update sharing.page.ts

--- src/app/sharing/sharing.page.ts
+++ src/app/sharing/sharing.page.ts
@@ -11,55 +11,20 @@ import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 })
 export class SharingPage {
  
-  text = 'Check out the Ionic Academy!';
-  url = 'https://ionicacademy.com';
+  text:string = null;
+  url:string = null;
+  img:string = null;
+  file:string = null;
+  
+  constructor(private socialSharing: SocialSharing) { }
  
-  constructor(private socialSharing: SocialSharing, private file: File) { }
- 
-  async shareTwitter() {
+  share() {
     // Either URL or Image
-    this.socialSharing.shareViaTwitter(null, null, this.url).then(() => {
+    this.socialSharing.share(this.text, this.url, this.img, this.file).then(() => {
       // Success
-    }).catch((e) => {
+    }).catch(( ) => {
       // Error!
     });
-  }
  
-  async shareWhatsApp() {   
-    // Text + Image or URL works
-    this.socialSharing.shareViaWhatsApp(this.text, null, this.url).then(() => {
-      // Success
-    }).catch((e) => {
-      // Error!
-    });
-  }
- 
-  async resolveLocalFile() {
-    return this.file.copyFile(`${this.file.applicationDirectory}/assets/`, 'kor.jpg', this.file.cacheDirectory, `${new Date().getTime()}.jpg`);
-  }
- 
-  removeTempFile(name) {
-    this.file.removeFile(this.file.cacheDirectory, name);
-  }
- 
-  async shareEmail() {
-    let file = await this.resolveLocalFile();
- 
-    this.socialSharing.shareViaEmail(this.text, 'My custom subject', ['arsyadshidiq1.com'], null, null, file.nativeURL).then(() => {
-      this.removeTempFile(file.name);
-    }).catch((e) => {
-      // Error!
-    });
-  }
- 
-  async shareFacebook() {
-    let file = await this.resolveLocalFile();
- 
-    // Image or URL works
-    this.socialSharing.shareViaFacebook(null, file.nativeURL, null).then(() => {
-      this.removeTempFile(file.name);
-    }).catch((e) => {
-      // Error!
-    });
-  }
+   }
 }
